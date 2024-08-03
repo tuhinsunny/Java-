55609e5ca8debb03e2a679c9012e00cc4ff9da7c
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-08-03 08:28:31 +0530

    feat: Add maths class with examples in Java

---
+++ maths.java
@@ -0,0 +1,11 @@
+public class maths {
+    public static void main(String[] args) {
+        //Maths class
+        System.out.println(Math.max(10, 20)); //20
+        System.out.println(Math.min(10, 20)); //10
+
+        //random
+        System.out.println((int)(Math.random()*100)); //random number between 0 and 100
+        
+    }
+}
